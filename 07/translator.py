aef63276b59504fad9292839f3989ad542af8dfd
John Pignata <john@pignata.com>
2019-01-06 14:56:37 -0500

    Minor tweaks to VM Translator
    - Use defaultdict constructor to add commands
    - Rename some variables for clarity (e.g. length -> position)
    - Start position at -1 rather than subtracting 1 when passing
    - Use function that returns a nested function rather than an object
      for the defaultdict null factory
    - Add constructor with default arity of self and two args to all
      commands instead of branching on command name
    - Don't use named captures since it's pretty clear from the expression
    - Add explicit comment method to Writer for clarity

--- 07/translator.py
+++ 07/translator.py
@@ -7,8 +7,8 @@ class Writer:
     def __init__(self, f):
         self.file = f
 
-    def line(self, line):
-        self.file.write(f'{line}\n')
+    def comment(self, line):
+        self.file.write(f'// {line}\n')
 
     def lines(self, lines):
         self.file.write('\n'.join(lines))
@@ -20,13 +20,13 @@ class Command:
                     'that': 'THAT'}
     POINTER = '@THIS', '@THAT'
 
-    def null():
-        return Null
+    def __init__(self, arg1, arg2):
+        pass
 
     def length(self):
         return len(self.asm())
 
-    def compare(self, method, position=0):
+    def compare(self, method, position):
         return ('@SP',
                 'AM=M-1',
                 'D=M',
@@ -57,103 +57,88 @@ class Command:
                 'M=M+1')
 
 
-class Null(Command):
-    def asm(self, namespace=None, position=None):
-        raise NotImplementedError
-
-
 class Push(Command):
     def __init__(self, segment, index):
         self.segment = segment
-        self.index = index
+        self.index = int(index)
 
     def asm(self, namespace='Default', position=None):
-        statements = [f'@{self.index}',
-                      'D=A']
+        statements = (f'@{self.index}',
+                      'D=A')
 
         if self.segment in self.BASE_ADDRESS:
-            symbol = self.BASE_ADDRESS[self.segment]
-            statements += [f'@{symbol}',
+            statements += (f'@{self.BASE_ADDRESS[self.segment]}',
                            'A=M+D',
-                           'D=M']
+                           'D=M')
         elif self.segment == 'temp':
-            statements += ['@R5',
+            statements += ('@R5',
                            'A=A+D',
-                           'D=M']
+                           'D=M')
         elif self.segment == 'pointer':
-            pointer = self.POINTER[self.index]
-            statements += [pointer,
-                           'D=M']
+            statements += (self.POINTER[self.index],
+                           'D=M')
         elif self.segment == 'static':
-            qualified_symbol = f'@{namespace}.{self.index}'
-            statements += [qualified_symbol,
-                           'D=M']
+            statements += (f'@{namespace}.{self.index}',
+                           'D=M')
 
-        statements += ['@SP',
-                       'A=M',
-                       'M=D',
-                       '@SP',
-                       'M=M+1']
-
-        return statements
+        return statements + ('@SP',
+                             'A=M',
+                             'M=D',
+                             '@SP',
+                             'M=M+1')
 
 
 class Pop(Command):
     def __init__(self, segment, index):
         self.segment = segment
-        self.index = index
+        self.index = int(index)
 
     def asm(self, namespace='Default', position=None):
-        statements = [f'@{self.index}',
-                      'D=A']
+        statements = (f'@{self.index}',
+                      'D=A')
 
         if self.segment in self.BASE_ADDRESS:
-            symbol = self.BASE_ADDRESS[self.segment]
-            statements += [f'@{symbol}',
+            statements += (f'@{self.BASE_ADDRESS[self.segment]}',
                            'D=M+D',
                            '@R13',
-                           'M=D']
+                           'M=D')
         elif self.segment == 'temp':
-            statements += ['@R5',
+            statements += ('@R5',
                            'D=A+D',
                            '@R13',
-                           'M=D']
+                           'M=D')
         elif self.segment == 'pointer':
-            pointer = self.POINTER[self.index]
-            statements += [pointer,
+            statements += (self.POINTER[self.index],
                            'D=A',
                            '@R13',
-                           'M=D']
+                           'M=D')
         elif self.segment == 'static':
-            qualified_symbol = f'@{namespace}.{self.index}'
-            statements += [qualified_symbol,
+            statements += (f'@{namespace}.{self.index}',
                            'D=A',
                            '@R13',
-                           'M=D']
+                           'M=D')
 
-        statements += ['@SP',
-                       'AM=M-1',
-                       'D=M',
-                       '@R13',
-                       'A=M',
-                       'M=D']
-
-        return statements
+        return statements + ('@SP',
+                             'AM=M-1',
+                             'D=M',
+                             '@R13',
+                             'A=M',
+                             'M=D')
 
 
 class EQ(Command):
-    def asm(self, namespace=None, position=0):
-        return self.compare('EQ', position=position)
+    def asm(self, namespace=None, position=None):
+        return self.compare('EQ', position)
 
 
 class LT(Command):
-    def asm(self, namespace=None, position=0):
-        return self.compare('LT', position=position)
+    def asm(self, namespace=None, position=None):
+        return self.compare('LT', position)
 
 
 class GT(Command):
-    def asm(self, namespace=None, position=0):
-        return self.compare('GT', position=position)
+    def asm(self, namespace=None, position=None):
+        return self.compare('GT', position)
 
 
 class Add(Command):
@@ -164,16 +149,6 @@ class Add(Command):
 class Sub(Command):
     def asm(self, namespace=None, position=None):
         return self.operate('-')
-
-
-class And(Command):
-    def asm(self, namespace=None, position=None):
-        return self.operate('&')
-
-
-class Or(Command):
-    def asm(self, namespace=None, position=None):
-        return self.operate('|')
 
 
 class Neg(Command):
@@ -187,6 +162,16 @@ class Neg(Command):
                 'M=M+1')
 
 
+class And(Command):
+    def asm(self, namespace=None, position=None):
+        return self.operate('&')
+
+
+class Or(Command):
+    def asm(self, namespace=None, position=None):
+        return self.operate('|')
+
+
 class Not(Command):
     def asm(self, namespace=None, position=None):
         return ('@SP',
@@ -197,41 +182,30 @@ class Not(Command):
                 'M=M+1')
 
 
-pattern = re.compile(r'(?P<command>\w+) ?(?P<arg1>\w+)? ?(?P<arg2>\d+)?')
-commands = defaultdict(Command.null)
+def null():
+    def _(arg1, arg2):
+        raise NotImplementedError
 
-commands['push'] = Push
-commands['pop'] = Pop
-commands['eq'] = EQ
-commands['lt'] = LT
-commands['gt'] = GT
-commands['add'] = Add
-commands['sub'] = Sub
-commands['neg'] = Neg
-commands['and'] = And
-commands['or'] = Or
-commands['not'] = Not
+    return _
 
-with open(sys.argv[1]) as infile:
-    fname = sys.argv[1].split('/')[-1].split('.')[0]
 
-    with open(sys.argv[2], 'w') as outfile:
-        writer = Writer(outfile)
-        length = 0
+pattern = re.compile(r'(\w+) ?(\w+)? ?(\d+)?')
+commands = defaultdict(null, {'push': Push, 'pop': Pop, 'eq': EQ, 'lt': LT,
+                              'gt': GT, 'add': Add, 'sub': Sub, 'neg': Neg,
+                              'and': And, 'or': Or, 'not': Not})
+namespace = sys.argv[1].split('/')[-1].split('.')[0]
+position = -1
 
-        for line in infile:
-            current_line = line.strip()
+with open(sys.argv[2], 'w') as outfile:
+    writer = Writer(outfile)
 
-            if len(current_line) > 0 and current_line[0] != '/':
-                writer.line(f'// {current_line}')
+    with open(sys.argv[1]) as infile:
+        for line in [line.strip() for line in infile]:
+            if len(line) > 0 and line[0] != '/':
+                cmd, arg1, arg2 = pattern.match(line).group(1, 2, 3)
+                command = commands[cmd](arg1, arg2)
+                asm = command.asm(namespace=namespace, position=position)
+                position += command.length()
 
-                matches = pattern.match(current_line)
-                cmd, arg1, arg2 = matches.group('command', 'arg1', 'arg2')
-
-                if cmd == 'push' or cmd == 'pop':
-                    command = commands[cmd](arg1, int(arg2))
-                else:
-                    command = commands[cmd]()
-
-                writer.lines(command.asm(namespace=fname, position=length - 1))
-                length += command.length()
+                writer.comment(line)
+                writer.lines(asm)
